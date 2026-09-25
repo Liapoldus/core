2ab6a6163194ac58fb16e0f25babca713d2b4ea4
Oleg <haitsynoleg@yandex.by>
2026-09-25 22:58:59 +0300

    refactor(core): удалить осиротевшую авторизацию

--- internal/domain/interfaces/authorizer.go
+++
@@ -1,7 +0,0 @@
-package interfaces
-
-import "github.com/Liapoldus/core/internal/domain/models"
-
-type Authorizer interface {
-	Authorize(models.Actor, string) error
-}
--- internal/domain/models/actor.go
+++
@@ -1,5 +0,0 @@
-package models
-
-type Actor struct {
-	ID string
-}
